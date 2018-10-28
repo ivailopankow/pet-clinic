c81609a3752766b5b5246d2fe71b19e86bb9b9fa
Ivaylo Pankov <ipankov@vmware.com>
2018-10-28 20:22:14 +0200

    Extracts common service

--- pet-clinic-data/src/main/java/home/ivy/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/home/ivy/petclinic/services/PetService.java
@@ -4,11 +4,5 @@ import home.ivy.petclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
